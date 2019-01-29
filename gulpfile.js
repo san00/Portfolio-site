e8e253dd39ac6ddc3bfd78118350c77e6a581be2
anslem herelle <anslemherelle@anslems-iMac.local>
2019-01-29 20:27:12 +0000

    Update css stylesheet

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
-const browserSync = require('browser-sync').create();
-const uglify = require('gulp-uglify');
-const imagemin = require('gulp-imagemin');
-const deploy = require('gulp-gh-pages');
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+const browserSync = require("browser-sync").create();
+const uglify = require("gulp-uglify");
+const imagemin = require("gulp-imagemin");
+const deploy = require("gulp-gh-pages");
 
 //  -- Top level functions --
 //  gulp.task - defines tasks
@@ -13,39 +13,48 @@ const deploy = require('gulp-gh-pages');
 //  gulp.watch - watch files & folders for changes
 
 // Copy all html files
-gulp.task('html', function() {
-  gulp.src('src/*.html').pipe(gulp.dest('dist'));
+gulp.task("html", function() {
+  gulp.src("src/*.html").pipe(gulp.dest("dist"));
 });
 
 //Copy & minify javascript files
-gulp.task('javascript', function() {
-  gulp.src('src/js/*.js').pipe(uglify()).pipe(gulp.dest('dist/js'));
+gulp.task("javascript", function() {
+  gulp
+    .src("src/js/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("dist/js"));
 });
 
 //  Optimise images
-gulp.task('imagemin', () =>
-    gulp.src('src/images/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('dist/images'))
+gulp.task("imagemin", () =>
+  gulp
+    .src("src/images/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("dist/images"))
 );
 
 // Compile sass & autoprefix
-gulp.task('sass', function() {
-  return gulp.src(['src/sass/*.scss']).pipe(sass().on('error', sass.logError)).pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false})).pipe(gulp.dest('src/css')).pipe(browserSync.stream());
+gulp.task("sass", function() {
+  return gulp
+    .src(["src/sass/*.scss"])
+    .pipe(sass().on("error", sass.logError))
+    .pipe(autoprefixer({ browsers: ["last 2 versions"], cascade: false }))
+    .pipe(gulp.dest("dist/css"))
+    .pipe(browserSync.stream());
 });
 
 //deploy to github pages
-gulp.task('deploy', () => {return gulp.src('dist/**/*').pipe(deploy())});
+gulp.task("deploy", () => {
+  return gulp.src("dist/**/*").pipe(deploy());
+});
 
 // -- Watch & serve tasks --
 
-gulp.task('default', [
-  'sass', 'javascript', 'html', 'imagemin'
-], function() {
-  browserSync.init({server: './src'});
+gulp.task("default", ["sass", "javascript", "html", "imagemin"], function() {
+  browserSync.init({ server: "./src" });
 
-  gulp.watch('src/js/*.js', ['javascript']).on('change', browserSync.reload);
-  gulp.watch('src/sass/*.scss', ['sass']);
-  gulp.watch('src/*.html').on('change', browserSync.reload);
-  gulp.watch('src/*.html', ['html']);
+  gulp.watch("src/js/*.js", ["javascript"]).on("change", browserSync.reload);
+  gulp.watch("src/sass/*.scss", ["sass"]);
+  gulp.watch("src/*.html").on("change", browserSync.reload);
+  gulp.watch("src/*.html", ["html"]);
 });
